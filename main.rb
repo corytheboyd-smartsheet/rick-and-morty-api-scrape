eb0a09e225686130fb07ca62e9a57b4a06da3003
Cory Boyd <cory.boyd@smartsheet.com>
2022-08-04 14:06:34 -0600

    Addendum, documentation

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
+# See: https://bundler.io/guides/bundler_setup.html
+require 'rubygems'
 require 'bundler/setup'
-
 require 'httparty'
 
 threads = 42.times.map do |i|
+  # See: https://ruby-doc.org/core-2.7.3/Thread.html
   Thread.new do
     response = HTTParty.get("https://rickandmortyapi.com/api/character?page=#{i+1}")
     puts response
   end
 end
 
+# See: https://ruby-doc.org/core-2.7.3/Thread.html#join-method
 threads.each(&:join)
